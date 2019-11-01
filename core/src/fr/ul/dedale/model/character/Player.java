4a6219a0fb6a63424cdf32efb39201b7b76fe54c
Ambrozik <hug.a@hotmail.fr>
2019-11-01 21:16:09 +0100

    juste cahngé la class DirectionFactory pour avoir un enum

--- core/src/fr/ul/dedale/model/character/Player.java
+++ core/src/fr/ul/dedale/model/character/Player.java
@@ -7,35 +7,13 @@ import fr.ul.dedale.DataFactory.TextureFactory;
 public class Player extends Character {
     public static int CPTANIMATION = 4;
     public static int SPRITESIZE = 30  ;
+    ;
+
 
     public Player(int x , int y ){
         super.posX = x;
         super.posY = y;
         super.sprite = new Sprite(TextureFactory.getInstance().getImage("hero"),SPRITESIZE,SPRITESIZE);
         sprite.setSize(4,4);
-    }
-
-    /**
-     * Decrease the Health point of the player
-     * @param damage Damage the player take
-     */
-    public void decreaseHp(int damage) {
-        hp = hp - damage;
-    }
-
-    /**
-     * Set the abscissa of the player
-     * @param x the value of the set
-     */
-    public void setX(int x){
-        posX = x;
-    }
-
-    /**
-     * Set the ordinate of the player
-     * @param y the value of the set
-     */
-    public void setY(int y) {
-        posY = y;
     }
 }
