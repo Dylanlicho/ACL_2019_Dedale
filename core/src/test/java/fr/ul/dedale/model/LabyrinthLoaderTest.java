d7829764cd9739b805746b119994efb63f2c926b
Angela Ipseiz <angela.ipseiz8@etu.univ-lorraine.fr>
2019-11-28 18:37:38 +0100

    changement du gradle pour pouvoir compiler avec les tests JUnit

---
+++ core/src/test/java/fr/ul/dedale/model/LabyrinthLoaderTest.java
@@ -0,0 +1,17 @@
+package fr.ul.dedale.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class LabyrinthLoaderTest {
+
+
+    @Test
+    public void test()  {
+        boolean b = true;
+        assertTrue("Booleen incorrect", b);
+    }
+
+}
